ec28144d1fb9faa1ec072708e3e1fdf94717bc37
Grigorii Matiukhin <contact@gmatiukhin.site>
2023-10-03 13:54:48 +0300

    pid logic

--- src/main.py
+++ src/main.py
@@ -1,18 +1,11 @@
 from machine import Pin
 from utime import sleep
 from sys import exit, stdin
+import math
 
 
 led = Pin(25, Pin.OUT)
 led.value(1)
-
-
-def front_encoder_handler(pin):
-    print(f"front encoder interrupt for {pin}; val={pin.value()}")
-
-
-def back_encoder_handler(pin):
-    print(f"back encoder interrupt for {pin}; val={pin.value()}")
 
 
 class Wheel:
@@ -23,21 +16,26 @@ class Wheel:
         self.enc_a = Pin(enc_a, Pin.IN, Pin.PULL_UP)
         self.enc_b = Pin(enc_b, Pin.IN, Pin.PULL_UP)
 
-        self.enc_a.irq(trigger=Pin.IRQ_RISING, handler=front_encoder_handler)
-        self.enc_b.irq(trigger=Pin.IRQ_RISING, handler=back_encoder_handler)
+        self.enc_a.irq(trigger=Pin.IRQ_RISING, handler=self.read_encoder)
+
+        self.pos_inc = 0
+
+    def read_encoder(self, pin):
+        enc_b_val = self.enc_b.value()
+        self.pos_inc += 1 if enc_b_val > 0 else -1
 
     def forward(self):
         print("hi")
-        self.fwd.value(1)
-        self.bwd.value(0)
+        self.fwd.high()
+        self.bwd.low()
 
     def backward(self):
-        self.fwd.value(0)
-        self.bwd.value(1)
+        self.fwd.low()
+        self.bwd.high()
 
     def stop(self):
-        self.fwd.value(0)
-        self.bwd.value(0)
+        self.fwd.low()
+        self.bwd.low()
 
 
 bow_port = Wheel(0, 1, 2, 3)
@@ -47,6 +45,27 @@ bow_starboard = Wheel(8, 9, 10, 11)
 stern_starboard = Wheel(12, 13, 14, 15)
 
 wheels = {"bp": bow_port, "sp": stern_port, "bs": bow_starboard, "ss": stern_starboard}
+
+
+kp = 1
+kd = 0.025
+ki = 0.0
+
+
+def pid_control(pos, target, delta_t, prev_error, integral_error):
+    error = pos - target
+    d_error = (error - prev_error) / delta_t
+    integral_error += error * delta_t
+
+    # control signal
+    u = kp * error + kd * d_error + ki * integral_error
+
+    pwr = max(math.fabs(u), 255)
+    dir = 1 if u > 0 else -1
+
+    # set motor
+    # store error
+
 
 cmd = None
 while True:
